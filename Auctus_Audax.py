a422a20769e52ebff0dbf46f88638328e53c2f7d
Star-Tube <50463083+Star-Tube@users.noreply.github.com>
2023-02-05 06:55:33 +1100

    Net Rev Sum Optomization
    Dict Comp should be faster here though I'm not certain... probably worth testing before adopting since I admit this is 100 less readable. Has been tested to give the same result though.

--- Auctus_Audax.py
+++ Auctus_Audax.py
@@ -78,13 +78,7 @@ def send_resources(list_of_net_revenues):
                 session = bank_withdraw(net_rev, session)
 
 def sum_of_net_revs(list_of_net_revenues):
-    #given list of net revs, return one sum net rev dict
-    net_production = {"money" : 0, "coal": 0, "oil": 0, "uranium": 0, "lead": 0, "iron": 0, "bauxite": 0, "gasoline": 0, "munitions": 0, "steel": 0, "aluminum": 0, "food": 0}
-    for net_rev in list_of_net_revenues:
-        for key, value in net_rev.items():
-            if key != 'nation' and key != 'nation_name' and key != 'send':
-                net_production[key] += net_rev[key]
-    return net_production
+    return {k: sum(net_rev[k] for net_rev in list_of_net_revenues if k in net_rev and k) for k in set(k for d in list_of_net_revenues for k in d if k not in ('nation', 'nation_name', 'send'))}
     
 
 def audit_given_nations(api_data):
